f501fbdf680581320a6d070a8bd11de79dc14397
Steve Yu <soundedrain@gmail.com>
2021-05-18 08:25:25 +0800

    [add] bit experiment WIP

---
+++ src/components/Button/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="button">{{ buttonText }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    buttonText: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="less" src="./style.less"></style>
